43196e69511f604e047ccf58ff2644776b7e4271
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-09 14:44:54 -0600

    adds level to cat level function

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@ var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
 	this.level = ko.computed(function() {
-		if (this.clickCount() < 10)
-			return "newborn";
-		else
-			return "infant";
+		var level;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			level = "newborn";
+		} else if (clicks < 50) {
+			level = "infant";
+		} else if (clicks < 100) {
+			level = "child";
+		} else if (clicks < 200) {
+			level = "teen";
+		} else {
+			level = "adult";
+		}
+		return level;
 	}, this);
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 	this.nicknames = ko.observableArray(["tabs",
