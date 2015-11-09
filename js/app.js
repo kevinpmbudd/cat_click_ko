177ad2fb8688ffaee7eadb3e0a8fdb67e917f36c
kbudd1234 <kevinpmbudd@gmail.com>
2015-11-09 14:38:02 -0600

    adds array of nicknames for cat

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ var ViewModel = function() {
 			return "infant";
 	}, this);
 	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.nicknames = ko.observableArray(["tabs",
+																			 "T-bone",
+																			 "Tabby Hoffman",
+																			 "Sir Tabs Alot",
+																			 "Claw foot tab"]);
 
 	this.incrementCounter = function() {
 		this.clickCount(this.clickCount() + 1);
